f1e9a50ecfac57791bea1155d6821e5d9f7e99bf
MahmoudSiliman <modysiliman75@gmail.com>
2020-12-28 00:36:04 +0200

    creat project structure

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul v-for="resourse in storedResourses" :key="resourse.id"> 
+    <li> <strong>ID:</strong> {{resourse.id}}  </li>
+    <li> <strong> Title: </strong> {{resourse.title}} </li>
+    <li> <strong> Description: </strong> {{resourse.description}} </li>
+    <li> <strong> Llink: </strong> <a :href="resourse.link">{{resourse.link}}</a> </li> 
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResourses: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js Documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn To Google.....',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.component(App);
+app.mount('#app');
