e05183b7ef20cf3e9ddeb090188ac8c493329023
MahmoudSiliman <modysiliman75@gmail.com>
2020-12-29 23:54:52 +0200

    add and style a base button component

--- src/components/learning-resourses/LearningResourse.vue
+++ src/components/learning-resourses/LearningResourse.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3> {{title}} </h3>
-        <button> Delete </button>
+        <base-button mode='flat'> Delete </base-button>
       </header>
       <p>{{description}}</p>
       <nav>
@@ -17,7 +17,13 @@
 
 export default {
   props: [ 'title', 'description', 'link' ],
+  data() {
+    return {
+      deleteIt: false,
+    };
+  },
 }
+
 </script>
 
 <style scoped>
